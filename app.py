648d5eea45a2b6c52cb356b6e7d8559294afde41
samin <saminsakur06@gmail.com>
2021-09-15 15:09:52 +0600

    added flash message

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ app.config['SECRET_KEY'] = "fsht8thbgnvgfthre"
 app.register_blueprint(views, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/')
 
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
