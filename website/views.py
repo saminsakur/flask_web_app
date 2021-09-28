3414ee10eca8c8a2d4af025d7e0331f3b5e77f76
samin <saminsakur06@gmail.com>
2021-09-28 23:55:09 +0600

    Initial commit

--- website/views.py
+++ website/views.py
@@ -1,13 +1,25 @@
 from .models import Note
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from flask.globals import request
 from flask.helpers import flash
 from flask_login import login_required, current_user
 from . import db
+import json
 
 
 views = Blueprint(__name__, "views")
 
+@views.route("/delete-note", methods=['POST'])
+def delete_note():
+    data = json.loads(request.data)
+    note_id = data['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
 
 @views.route("/", methods=["GET", "POST"])
 @login_required
